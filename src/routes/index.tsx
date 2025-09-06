04ea17ca3f0d63a6dfed5e93ebd2165547d7cb68
Ewan <ejdankesreither@gmail.com>
2025-09-06 13:59:36 +1000

    I got literally npwhere with this

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,46 @@
 import { createFileRoute } from "@tanstack/solid-router";
-import { SpotifyWebApi } from '@spotify/web-api-ts-sdk';
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import { Suspense, createResource } from "solid-js";
 
 export const Route = createFileRoute("/")({
   component: IndexComponent,
 });
 
+let sdk: SpotifyApi | undefined;
+// (
+//   import.meta.env.VITE_PUBLIC_CLIENT_ID,
+//   window.location.href,
+//   [
+//     "playlist-read-private",
+//     "user-modify-playback-state",
+//     "streaming",
+//     "user-read-email",
+//     "user-read-private",
+//   ],
+//   window.location.href
+// );
+
+const handler = () => {
+  sdk = SpotifyApi.performUserAuthorization(
+    import.meta.env.VITE_PUBLIC_CLIENT_ID,
+    window.location.href,
+    [
+      "playlist-read-private",
+      "user-modify-playback-state",
+      "streaming",
+      "user-read-email",
+      "user-read-private",
+    ],
+    window.location.href,
+  );
+};
+
+const [search] = createResource(() => sdk?.search("beatles", ["artist"]));
 function IndexComponent() {
   return (
     <div class="flex items-center justify-center bg-gray-500 h-screen w-full">
-        <button class="w-50 text-2xl bg-pink-600">Log in?</button>
+      <button class="w-50 text-2xl bg-pink-600">Log in?</button>
+      <Suspense>{search()?.artists.items[0].name}</Suspense>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import solidPlugin from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: "solid", autoCodeSplitting: true }),
+    tanstackRouter({ target: "solid", autoCodeSplitting: true }),
     solidPlugin(),
     tailwindcss(),
   ],
